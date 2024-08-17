8c578f66e5c269d26315266a117efd7bb6b9de95
Jonathon_Bayliff <Jonathon.Bayliff@csuglobal.edu>
2024-08-17 14:00:24 -0600

    Add files via upload

---
+++ Module-1/CSC450_CT1_mod1-1_corrected.cpp
@@ -0,0 +1,22 @@
+
+/* Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.
+*/
+
+#include<iostream>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+    // Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
+
+    // Corrected Output Statement
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Return statement indicating successful execution
+    return 0;
+}
---
+++ Module-1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string firstName = "Chipper";
+    std::string lastName = "Jones";
+    std::string streetAddress = "1520 Coperstown Drive";
+    std::string city = "Cooperstown";
+    std::string zipCode = "65359";
+
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
+
+
+
+
